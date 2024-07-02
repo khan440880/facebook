521567d81def811d5add6fffc592e8de309d8915
khan440880 <165549408+khan440880@users.noreply.github.com>
2024-07-02 05:34:23 -0700

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,8 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+});
+
+document.getElementById('createAccount').addEventListener('click', function() {
+    alert('Create new account button clicked!');
+});
